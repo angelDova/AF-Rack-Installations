9d84085932232a71842fa1ac4cc619aa73328897
angelDova <Angelfcb01@yahoo.com>
2023-05-27 01:00:26 -0700

    update

--- components/ServicesImg.jsx
+++ components/ServicesImg.jsx
@@ -6,11 +6,23 @@ import { FaInstagram } from "react-icons/fa";
 const ServicesImg = ({ services }) => {
   return (
     <div className="relative">
-      <Image alt="/" src={services} className="w-full h-full object-cover" />
+      <Image
+        alt="/"
+        src={services}
+        className="
+              object-cover 
+              h-full 
+              w-full 
+              group-hover:scale-110 
+              transition
+              ease-in-out
+              duration-300
+            "
+      />
       {/* Overlay */}
       <div className="flex justify-center w-full h-full items-center absolute top-0 left-0 right-0 bottom-0 hover:bg-black/50 group">
         <p className="text-gray-300 hidden group-hover:block">
-          <FaInstagram size={30} className="z-10" />
+          {/* <FaInstagram size={30} className="z-10" /> */}
         </p>
       </div>
     </div>
